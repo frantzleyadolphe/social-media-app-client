b4f7720362de21ec3ceb492d009f04033e14fe57
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-12-24 20:34:29 -0500

    added theme provider

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,16 +3,27 @@ import HomePage from "screens/home";
 import ProfilePage from "screens/profile";
 import NavBar from "screens/navbar";
 import LoginPage from "screens/login";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "../state/theme.js";
 
 const RouteNavigation = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <BrowserRouter>
       {/* <NavBar /> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
